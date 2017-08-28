c6651855ce96c2174723cce526f89e1f582c214f
Zach VanSomeren <23041336+zvansom@users.noreply.github.com>
2017-08-28 06:46:10 -0600

    JS working.  Initial style.  Index edited
    JS autocomplete and ajax call fully functional.

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
 $(document).ready(function(){
 
-  $("#searchButton").click(function(){
+// jQuery Shortcuts
+  var search_bar = $('#searchField');
+  var search_button = $('#searchButton');
+  var links = $('#results');
 
-    var searchField = $("#searchField").val();
-    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="
-              +searchField+"&callback=?"
 
-    $.ajax({
-      method: "GET",
-      url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=dog&callback=?",
-      datatype: "json",
-      success: function(result){
-        console.log(result);
-      },
-      error: function(err){
-        alert("There was an error.");
-      },
+// Link generator function for results window
+  function link_generator(data){
 
-    })
+    var i = 0,
+      title = data[1],
+      description = data[2],
+      link = data[3];
+
+    for(i; i < data[3].length; i++){
+      var  list_link = '<a href="'+link[i]+'" target="blank">'+title[i]+'</a><br/>';
+      var link_description = '<p class="description">'+description[i]+'</p>';
+      list_return = list_link+link_description+'<hr>';
+      links.append(list_return);
+    }
+  }
+
+// Wiki Auto-complete
+
+  search_bar.autocomplete({
+    source: function(query, result) {
+      $.ajax({
+        url: "https://en.wikipedia.org/w/api.php",
+        dataType: "jsonp",
+        data: {
+          'action': "opensearch",
+          'format': "json",
+          'search': query.term
+        },
+
+        success: function (data) {
+          links.empty();
+          link_generator(data);
+        }
+
+      });
+    }
 
   });
 
